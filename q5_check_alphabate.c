b8c35f323d888ba64c3a534b87861b88b35225a3
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-10-14 23:45:28 +0530

    Add Code Answer 5
    Check whether a character is an alphabet or not

---
+++ q5_check_alphabate.c
@@ -0,0 +1,18 @@
+//  Check whether a character is alphabet or not.
+
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Please Enter A Character:\n");
+    scanf("%c", &ch);
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+    {
+        printf("The Entered Character Is An Alphabate.");
+    }
+    else
+    {
+        printf("The Entered Character Is Not An Alphabate.");
+    }
+    return 0;
+}
